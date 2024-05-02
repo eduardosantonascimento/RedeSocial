8725e660d37c986deb6b4c4f09a626942fbab48d
Luiz Cruz <luiz.cruz@mercadolivre.com>
2024-05-02 17:50:37 -0300

    Refactor UserRepository class to use async/await and handle errors

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,13 +1,18 @@
 const httpStatus = require("../utils/statusCode");
 const ApiError = require("../utils/ApiError")
+const db = require("../models");
+const User = db.User;
 
 
 class UserRepository{
-  async getAll(){
-    return User.findAll(
+  getAll() {
+    try {
+      return User.findAll(
         { attributes: ['id', 'full_name', 'email',] }
-    );
-};
-}
+      );
+    } catch (error) {
+      throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, error.message);
+    }
+}};
 
 module.exports = UserRepository();
