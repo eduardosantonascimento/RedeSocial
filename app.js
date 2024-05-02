6809936b7fddb02fecca0b622d9ca9557e2e1e83
Luiz Cruz <luiz.cruz@mercadolivre.com>
2024-05-02 17:50:04 -0300

    Update userRoutes import in app.js

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 const express = require('express');
-const db = require("./src/database/models/db");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const userRoutes = require ('./src/routes/userRoutes');
+const userRoutes = require ('./src/routes/usersRoutes');
 
 app.use('/users', userRoutes);
 
-
 app.listen(port, () => {
-  console.log(`Example api listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 }); 
-
-/*console.log("Começou");
-
-(async () => {
-  
-  await db.insertCustomer({full_name:'Carolina Machado', email:'carolzinha18@gmail.com',password:'54321', is_active:'1'})
-  console.log('SELECT * FROM post');
-  const clientes = await db.selectCustomers();
-  console.log(clientes);
-})();
-
-*/
-
-module.exports = app;
